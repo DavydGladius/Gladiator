72703d5410902175864dfd7f92c024c81194386e
Mantas Skaudis <87621909+S-NicoX@users.noreply.github.com>
2026-02-22 19:05:08 +0200

    Animated Character Sprite
    + Updated main character to have an animated sprite (changes depenging on idling or waling)
    + Adjusted scale for map tiles
    + Ajusted character camera zoom
    + On main menu hid the settings button
    
    (Note for future scale and zoom might need to be adjusted!)

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,13 +1,27 @@
 extends CharacterBody2D
 
+@export var movement_speed = 100
+@onready var animations = $AnimatedSprite2D
 
-var movement_speed = 220.0
+func _physics_process(_delta):
+	player_movement()
 
+func player_movement():
 
-func _physics_process(delta: float) -> void:
-	playermovement()
-
-func playermovement():
-	var direction = Input.get_vector("left","right","up","down")
+	var direction = Input.get_vector("left", "right", "up", "down")
+	
 	velocity = direction * movement_speed
 	move_and_slide()
+	
+	update_animations(direction)
+
+func update_animations(direction):
+	if direction != Vector2.ZERO:
+		animations.play("walk")
+		
+		if direction.x > 0:
+			animations.flip_h = false
+		elif direction.x < 0:
+			animations.flip_h = true
+	else:
+		animations.play("idle")
