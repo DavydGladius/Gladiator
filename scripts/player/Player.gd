3fbcb1913a80488107bc7ca0e3c0c89f20b47459
Mantas Skaudis <87621909+mskaudis@users.noreply.github.com>
2026-03-04 12:42:03 +0200

    Case sensetivity fix
    + After death game won't go to main menu in the export version. It was a case sensetive issue

--- scripts/player/Player.gd
+++ scripts/player/Player.gd
@@ -2,12 +2,13 @@ extends Entity
 
 func _ready():
 	super._ready()
-	# Entity handled the animation, now we just change scenes
 	died.connect(_on_player_died)
 
 func _physics_process(_delta):
+	if is_dead: return 
+	
 	var direction = Input.get_vector("left", "right", "up", "down")
 	handle_movement(direction)
 
 func _on_player_died():
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
