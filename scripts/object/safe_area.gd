894d29bb14267d349f41ca3d311016836c176c27
DavydGladius <dovydasprosegalavicius@gmail.com>
2026-03-29 19:02:01 +0300

    file managing
    changed locations of missorted files

--- scripts/object/safe_area.gd
+++ scripts/object/safe_area.gd
@@ -1,10 +1,8 @@
 extends Area2D
 
-# Naudojame @onready, kad rastume WaveManager per tėvą (World)
 @onready var wave_manager = get_parent().get_node("WaveManager")
 
 func _ready():
-	# Sujungiame signalus, kad jie veiktų automatiškai
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
 
@@ -14,13 +12,11 @@ func _on_body_entered(body):
 		$CanvasLayer.visible = true
 		if wave_manager:
 			wave_manager.stop_wave()
-			wave_manager.clear_enemies() # Čia ištrinami priešai
-		body.modulate.a = 0.5
+			wave_manager.clear_enemies()
 
 func _on_body_exited(body):
 	if body.is_in_group("player"):
 		print("Žaidėjas paliko saugią zoną!")
 		$CanvasLayer.visible = false
 		if wave_manager:
-			wave_manager.restart_current_wave() # Čia banga prasideda iš naujo
-		body.modulate.a = 1.0
+			wave_manager.restart_current_wave()
